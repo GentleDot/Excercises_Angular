f9e581d35857a1a4b07450acea2e6bf13d9d1540
gentledot <gentledot.wp@gmail.com>
2019-07-30 02:31:02 +0900

    연산 / 문제1 : [(ngModel)]과 formcontrol은 혼용할 수 없음.

--- src/app/operation/square-feet-and-meters/square-feet-and-meters.component.ts
+++ src/app/operation/square-feet-and-meters/square-feet-and-meters.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from "@angular/core";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Component, OnInit} from "@angular/core";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Converter} from "../converter.enum";
 
 @Component({
@@ -12,18 +12,39 @@ export class SquareFeetAndMetersComponent implements OnInit {
 
   length: number;
   width: number;
+  squareFeet: number;
   converter: number = Converter.feetToMeter;
 
-  squareForm = new FormGroup({});
+  squareForm = new FormGroup({
+    firstNum: new FormControl(""),
+    secondNum: new FormControl(""),
+  });
 
   constructor(private fb: FormBuilder) {
     this.squareForm = fb.group({
-      firstNum : ["", [Validators.required, Validators.min(1), Validators.pattern(/^\d+$/)]],
-      secondNum : ["", [Validators.required, Validators.min(1), Validators.pattern(/^\d+$/)]],
+      firstNum: ["", [Validators.required, Validators.min(1), Validators.pattern(/^\d+$/)]],
+      secondNum: ["", [Validators.required, Validators.min(1), Validators.pattern(/^\d+$/)]],
     });
   }
 
   ngOnInit() {
+    this.squareForm.controls.firstNum.valueChanges
+      .subscribe(input => {
+        this.length = input;
+      });
+    this.squareForm.controls.secondNum.valueChanges
+      .subscribe(input => {
+        this.width = input;
+      })
+  }
+
+  onCal() {
+    this.squareFeet = Number(this.length) * Number(this.width)
+    return this.squareFeet;
+  }
+
+  onConvert() {
+    return this.squareFeet * this.converter;
   }
 
 }
